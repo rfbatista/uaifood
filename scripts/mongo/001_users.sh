ec858e6c1ab20aa5c2950233ed998b3780f69f2e
rfbatista <renfbatista@gmail.com>
2022-04-26 08:46:30 -0300

    ci: mongo docker scripts

---
+++ scripts/mongo/001_users.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+db.createUser(
+  {
+    user: '$MONGO_INITDB_ROOT_USERNAME',
+    pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+    roles: [{ role: 'readWrite', db: '$MONGO_INITDB_DATABASE' }],
+  },
+);
+EOF
