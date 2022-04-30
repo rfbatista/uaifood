090390c6be88ee837c76c53b6c6073172deaf1df
rfbatista <renfbatista@gmail.com>
2022-04-29 23:16:47 -0300

    feat: postgres datasource

---
+++ src/infrastructure/data-source/postgres/postgres.ts
@@ -0,0 +1,19 @@
+import { config } from '@infrastructure/config';
+import { DataSource } from 'typeorm';
+import { RestaurantSchema } from '@infrastructure/data-source/schemas/restaurant-schema';
+import { ItemSchema } from '@infrastructure/data-source/schemas/item-schema';
+
+const postgres = new DataSource({
+  type: 'postgres',
+  host: config.dataSource.postgres.host,
+  port: config.dataSource.postgres.port,
+  database: config.dataSource.postgres.database,
+  password: config.dataSource.postgres.password,
+  username: config.dataSource.postgres.user,
+  synchronize: false,
+  logging: true,
+  entities: [RestaurantSchema, ItemSchema],
+  migrations: ['migration/**/*.ts'],
+});
+
+export { postgres };
