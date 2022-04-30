df206e09bdf0076e446bba7df5964b93378382d9
rfbatista <renfbatista@gmail.com>
2022-04-29 23:14:57 -0300

    feat: price value object

--- src/core/domain/value-object/price.ts
+++ src/core/domain/value-object/price.ts
@@ -1,20 +1,24 @@
-import { createMoney, CurrencyEnum, Money } from '@core/domain/value-object/money';
+import { createMoney, CurrencyEnum } from '@core/domain/value-object/money';
 
 interface Price {
-	amount(): number
+  amount(): number;
+  currency(): CurrencyEnum;
   updateAmount(amount: number, currency?: CurrencyEnum): void;
 }
 
-const createPrice = (money: Money): Price => {
-  let currentMoney = money;
+const createPrice = (amount: number): Price => {
+  let currentMoney = createMoney(amount);
   return {
     amount: () => {
-      return money.amount
+      return currentMoney.amount;
+    },
+    currency: () => {
+      return currentMoney.currency;
     },
     updateAmount: (amount) => {
       currentMoney = createMoney(amount, CurrencyEnum.BRL);
-    }
-  }
-}
+    },
+  };
+};
 
 export { Price, createPrice };
