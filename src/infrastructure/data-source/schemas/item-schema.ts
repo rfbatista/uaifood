70fc708633fd6913a3169abe3e97981f14c2d11a
rfbatista <renfbatista@gmail.com>
2022-04-29 23:16:17 -0300

    feat: schemas

---
+++ src/infrastructure/data-source/schemas/item-schema.ts
@@ -0,0 +1,81 @@
+import { makeSchemaFromDomain, Schema } from '@infrastructure/data-source/schema';
+import { createItem, Item } from '@core/domain/entities/item';
+import { createPrice } from '@core/domain/value-object/price';
+import { createMoney, CurrencyEnum } from '@core/domain/value-object/money';
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { RestaurantSchema } from '@infrastructure/data-source/schemas/restaurant-schema';
+
+type ItemSchemaType = Schema & {
+  name: string;
+  restaurantId: string;
+  price: {
+    amount: number;
+    currency: string;
+  };
+};
+
+@Entity('item')
+class ItemSchema implements ItemSchemaType {
+  @PrimaryColumn({ type: 'uuid' })
+  id: string;
+  @Column()
+  name: string;
+  @Column('json')
+  price: {
+    amount: number;
+    currency: string;
+  };
+  @ManyToOne(() => RestaurantSchema, (item) => item.items)
+  @JoinColumn({ name: 'restaurant_id' })
+  restaurantId: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt?: Date;
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt?: Date;
+  constructor(props: ItemSchemaType) {
+    this.id = props?.id;
+    this.name = props?.name;
+    this.price = props?.price;
+    this.createdAt = props?.createdAt;
+    this.updatedAt = props?.updatedAt;
+    this.deletedAt = props?.deletedAt;
+    this.restaurantId = props?.restaurantId;
+  }
+}
+
+const itemToSchema = makeSchemaFromDomain<Item, ItemSchema>((domain: Item, schema: Schema): ItemSchema => {
+  return new ItemSchema({
+    ...schema,
+    restaurantId: domain.restaurantId,
+    name: domain.name || '',
+    price: {
+      amount: domain.price.amount(),
+      currency: domain.price.currency(),
+    },
+  });
+});
+
+const itemToDomain = (schema: ItemSchema) => {
+  createItem({
+    id: schema.id,
+    name: schema.name,
+    restaurantId: schema.restaurantId,
+    price: createPrice(schema.price.amount),
+    createdAt: schema.createdAt,
+    updatedAt: schema.updatedAt,
+    deletedAt: schema.deletedAt,
+  });
+};
+
+export { ItemSchema, itemToDomain, itemToSchema };
---
+++ src/infrastructure/data-source/schemas/restaurant-schema.ts
@@ -0,0 +1,92 @@
+import { createRestaurant, Restaurant } from '@core/domain/entities/restaurant';
+import { makeSchemaFromDomain, Schema } from '@infrastructure/data-source/schema';
+import { createCulinary } from '@core/domain/value-object/culinary';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryColumn,
+  UpdateDateColumn,
+  Column,
+  Entity,
+  OneToMany,
+  Index,
+} from 'typeorm';
+import { ItemSchema } from '@infrastructure/data-source/schemas/item-schema';
+import { Point } from 'geojson';
+import { ResultSuccess } from '@core/result';
+
+type RestaurantSchemaType = Schema & {
+  name: string;
+  culinary: string;
+  local?: Point;
+  city?: string;
+  items?: ItemSchema[];
+  document_with_weights?: unknown;
+};
+
+@Entity('restaurant')
+class RestaurantSchema implements RestaurantSchemaType {
+  @PrimaryColumn({ type: 'uuid' })
+  id: string;
+  @Column({ nullable: true })
+  name: string;
+  @Column()
+  @Index({ fulltext: true })
+  culinary: string;
+  @OneToMany(() => ItemSchema, (item) => item.restaurantId)
+  items?: ItemSchema[];
+  @Column({ type: 'json', nullable: true })
+  local?: Point;
+  @Column({ nullable: true })
+  city?: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt?: Date;
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt?: Date;
+
+  constructor(props: RestaurantSchema) {
+    this.id = props?.id;
+    this.name = props?.name;
+    this.city = props?.city;
+    this.local = props?.local;
+    this.culinary = props?.culinary;
+    this.createdAt = props?.createdAt;
+    this.updatedAt = props?.updatedAt;
+    this.deletedAt = props?.deletedAt;
+  }
+}
+
+const restaurantToSchema = makeSchemaFromDomain<Restaurant, RestaurantSchemaType>(
+  (domain: Restaurant, schema: Schema): RestaurantSchemaType => {
+    return new RestaurantSchema({
+      ...schema,
+      name: domain.name || '',
+      local: domain.address?.local && {
+        type: 'Point',
+        coordinates: [domain.address?.local?.long, domain.address?.local?.lat],
+      },
+      city: domain.address?.city,
+      culinary: domain.culinary.type,
+    });
+  },
+);
+
+const restaurantToDomain = (schema: RestaurantSchemaType) => {
+  const restaurant = createRestaurant({
+    id: schema.id,
+    name: schema.name,
+    culinary: createCulinary(schema.culinary),
+    address: {
+      local: schema.local && { lat: schema.local?.coordinates[1], long: schema.local?.coordinates[0] },
+      city: schema?.city,
+    },
+    createdAt: schema.createdAt,
+    updatedAt: schema.updatedAt,
+    deletedAt: schema.deletedAt,
+  });
+  return (restaurant as ResultSuccess<Restaurant>).data;
+};
+
+export { RestaurantSchema, restaurantToDomain, restaurantToSchema, RestaurantSchemaType };
