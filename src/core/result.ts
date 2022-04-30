c9fe49f492a4d197df711a454d383246f040d364
rfbatista <renfbatista@gmail.com>
2022-04-29 23:12:37 -0300

    feat: result dto

--- src/core/result.ts
+++ src/core/result.ts
@@ -1,20 +1,24 @@
-interface Result<Data> {
+interface Result {
   readonly isError: boolean;
   readonly isSuccess: boolean;
-  readonly data: Data;
-  readonly error?: Error;
 }
 
-interface ResultError extends Result<null> {}
+interface ResultError extends Result {
+  readonly data: unknown;
+}
 
-interface ResultSuccess<T> extends Result<T>{}
+interface ResultSuccess<T> extends Result {
+  readonly data: T;
+}
 
 const createErrorResult = (error: Error): ResultError => {
+  const message = error.message;
   return {
     isError: true,
     isSuccess: false,
-    data: null,
-    error,
+    data: {
+      error: [{ message }],
+    },
   };
 };
 
