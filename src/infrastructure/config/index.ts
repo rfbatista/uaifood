096fb4d534ce1f1075d1c020bb5886c6f88fa09d
rfbatista <renfbatista@gmail.com>
2022-04-29 23:18:00 -0300

    feat: config

---
+++ src/infrastructure/config/index.ts
@@ -0,0 +1,26 @@
+import { envNumber, envString } from '@infrastructure/config/environment';
+
+const config = {
+  http: {
+    port: envNumber('PORT', 3000),
+    host: envString('HOST', 'localhost'),
+  },
+  dataSource: {
+    mongodb: {
+      database: envString('MONGODB_NAME'),
+      host: envString('MONGODB_HOST'),
+      user: envString('MONGODB_USER'),
+      password: envString('MONGODB_PASSWORD'),
+      port: envNumber('MONGODB_PORT'),
+    },
+    postgres: {
+      database: envString('POSTGRES_NAME'),
+      user: envString('POSTGRES_USER'),
+      password: envString('POSTGRES_PASSWORD'),
+      host: envString('POSTGRES_HOST'),
+      port: envNumber('POSTGRES_PORT'),
+    },
+  },
+};
+
+export { config };
