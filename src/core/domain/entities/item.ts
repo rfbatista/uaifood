6d0379a17ebe9154185155fe332229f9dc614107
rfbatista <renfbatista@gmail.com>
2022-04-29 23:13:54 -0300

    feat: item domain

--- src/core/domain/entities/item.ts
+++ src/core/domain/entities/item.ts
@@ -1,25 +1,48 @@
 import { Price } from '@core/domain/value-object/price';
-import { createSuccessResult, ResultSuccess, ResultError } from '@core/result';
+import { createSuccessResult, ResultSuccess, ResultError, createErrorResult } from '@core/result';
 import { Entity, EntityType, makeEntity } from '../entity';
+import { UniqueIdentifier, createUniqueIdentifier } from '@core/domain/value-object/unique-identifier';
 
 type ItemType = EntityType & {
+  restaurantId: UniqueIdentifier;
   name?: string;
   price: Price;
 };
 
+type ItemDTO = {
+  id: string;
+  name: string;
+  price: number;
+};
 interface Item extends Entity {
   name?: string;
-  readonly price?: Price;
+  readonly restaurantId: UniqueIdentifier;
+  readonly price: Price;
+  toDTO: ItemDTO;
 }
 
 const createItem = makeEntity((entity, props: ItemType): ResultSuccess<Item> | ResultError => {
-  return createSuccessResult({
+  if (!props.price) return createErrorResult(new Error('Missing price for item'));
+  const restaurantId = createUniqueIdentifier(props.restaurantId);
+  const item = {
     ...entity,
+    get restaurantId() {
+      return restaurantId;
+    },
     name: props?.name,
     get price() {
       return props?.price;
     },
-  });
+    get toDTO() {
+      return {
+        id: entity.id,
+        name: props.name,
+        price: props.price.amount(),
+        createdAt: entity.createdAt,
+      };
+    },
+  };
+  return createSuccessResult(item);
 });
 
 export { ItemType, Item, createItem };
